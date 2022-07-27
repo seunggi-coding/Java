819d90b00aeceeab3ee2d12ddaeada0c6ea4eac8
seunggi-coding <moon010103@naver.com>
2022-07-27 09:33:51 +0900

    Java Practice

--- chap01/src/sec01/exam01/VariableInitializationExample.java
+++ chap01/src/sec01/exam01/VariableInitializationExample.java
@@ -6,6 +6,7 @@ public class VariableInitializationExample {
 		// TODO Auto-generated method stub
 		int value;
 		
+		// value값이 변수 선언하는 과정 혹은 그 직후에 초기화 되지 않고 사용되어 에러 발생
 		int result = value + 10;
 		
 		System.out.println(result);
