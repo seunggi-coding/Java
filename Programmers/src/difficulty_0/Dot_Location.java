82448de72b3c14487f336c2eb4b3dd8a83fec8c0
seunggi-coding <moon010103@naver.com>
2022-10-08 14:52:25 +0900

    practice

---
+++ Programmers/src/difficulty_0/Dot_Location.java
@@ -0,0 +1,10 @@
+package difficulty_0;
+
+public class Dot_Location {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Programmers/src/difficulty_0/letter.java
@@ -0,0 +1,24 @@
+package difficulty_0;
+
+public class letter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "happy birthday!";
+		System.out.println(solution(str));
+		
+	}
+	public static int solution(String message) {
+        int answer = 0;
+//        length()는 배열의 길이
+        char[] array = new char[message.length()];
+        
+//        length는 문자열의 길이
+        for(int i = 0; i < array.length; i++) {
+            array[i] = message.charAt(i);
+        }
+        answer = array.length*2;
+        return answer;
+    }
+
+}
