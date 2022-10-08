a130f20b98a6b2856bccea4e8847765178580a92
seunggi-coding <moon010103@naver.com>
2022-10-08 16:44:19 +0900

    practice

--- Programmers/src/difficulty_0/letter.java
+++ Programmers/src/difficulty_0/letter.java
@@ -10,10 +10,10 @@ public class letter {
 	}
 	public static int solution(String message) {
         int answer = 0;
-//        length()는 배열의 길이
+//        length()는 문자열의 길이
         char[] array = new char[message.length()];
         
-//        length는 문자열의 길이
+//        length는 배열의 길이
         for(int i = 0; i < array.length; i++) {
             array[i] = message.charAt(i);
         }
