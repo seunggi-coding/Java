9e134cb372cb0e22c91f0a68d58ac6516bdd82e5
seunggi-coding <moon010103@naver.com>
2022-10-10 20:04:20 +0900

    practice

---
+++ Programmers/src/difficulty_0/sort_odd.java
@@ -0,0 +1,17 @@
+package difficulty_0;
+
+public class sort_odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+		
+		System.out.println(solution(n));
+	}
+	public static int[] solution(int n) {
+        int[] answer = {};
+        
+        for(int i = 1)
+        return answer;
+    }
+}
